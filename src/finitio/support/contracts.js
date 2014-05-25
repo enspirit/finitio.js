9f962938ec9d78b77ee9c610fc649de901a952b5
Bernard Lambeau <blambeau@gmail.com>
2014-05-25 23:00:01 +0200

    Add a Function reusable contract

--- src/finitio/support/contracts.js
+++ src/finitio/support/contracts.js
@@ -89,6 +89,28 @@ $cs.Time = {
 
 };
 
+$cs.Function = {
+  defn: {
+
+    dress: function(args){
+      if (!/^return/.test(args[args.length-1])){
+        args[args.length-1] = 'return ' + args[args.length-1];
+      }
+      var fn = Function.apply(Function, args);
+      fn.defn = args;
+      return fn;
+    },
+
+    undress: function(fn){
+      if (!fn.defn){
+        throw new Error('No defn found: ' + fn.toString());
+      }
+      return fn.defn;
+    }
+
+  }
+}
+
 module.exports = {
   Contracts: $cs
 };
