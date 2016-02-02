15a3066e87c089e55fd1cdd429967666739417cb
Louis Lambeau <louislambeau@gmail.com>
2016-02-02 17:48:29 +0100

    Bump to 1.0.1

--- dist/finitio.js
+++ dist/finitio.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports={
   "name": "finitio",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "description": "Finitio is a language for capturing information structure.",
   "main": "index.js",
   "author": "Louis Lambeau <louislambeau@gmail.com>",
